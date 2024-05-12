77a6c2b43fa1434731c4e28ba2405612e9e4731f
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-05-11 19:07:36 -0600

    Primeros pasos del slider

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css'
+import Glide from '@glidejs/glide'
+import '@glidejs/glide/dist/css/glide.core.min.css'
+
+new Glide('#slider', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 1,
+  autoplay: 2000,
+  hoverpause: true,
+}).mount()
